f788a9c5a3791a26838aa181b44e061267c15918
Eric <51809267+Zhiwei-Lu@users.noreply.github.com>
2021-10-05 11:48:19 +0200

    Update FFT_My.py

--- FFT_My/FFT_My.py
+++ FFT_My/FFT_My.py
@@ -1,24 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 21 22:21:31 2021
-
-@author: luzhiwei
-"""
-
 import matplotlib.pyplot as plt 
 import numpy as np 
-def show(ori_func, ft, sampling_period = 5000): 
+def show(ori_func, ft, sampling_period = 5001*1E-18*100): 
     n = len(ori_func) 
-    print(n)
-    interval = sampling_period / n 
    
+    interval = sampling_period / n 
+    print(interval)
     plt.subplot(2, 1, 1) 
     plt.plot(np.arange(0, sampling_period, interval), ori_func, 'black') 
-    plt.xlabel('Time steps(x1000)'), plt.ylabel('Amplitude') 
+    plt.xlabel('Time steps'), plt.ylabel('Amplitude') 
 
     plt.subplot(2,1,2) 
-    frequency = np.arange(n / 2) / (n * interval) 
+    frequency = np.arange(n / 2-1) / (n * interval) 
+    plt.xlim(0,0.4*1E14)
     nfft = abs(ft[range(int(n / 2))] / n ) 
     plt.plot(frequency, nfft, 'red') 
     plt.xlabel('Freq (Hz)'), plt.ylabel('Amp. Spectrum') 
@@ -26,5 +19,6 @@ def show(ori_func, ft, sampling_period = 5000):
     
  
 x = np.loadtxt('M_y.txt')
+x=x
 y = np.fft.fft(x) 
 show(x,y)
